2973306126713fd3aeb8e1bd69676b0a8b19f443
zawette <zawette@gmail.com>
2023-11-26 03:09:04 +0100

    stateMachine and state base classes

---
+++ Shared/StateMachine/State.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class State : Node
+{
+
+	[Signal] public delegate void StateStartEventHandler();
+	[Signal] public delegate void StatePhysicsUpdateEventHandler();
+	[Signal] public delegate void StateExitEventHandler();
+	[Signal] public delegate void SwitchStateEventHandler(string stateName, Godot.Collections.Dictionary<string,Variant> message = null);
+
+
+	public virtual void OnEnter(Godot.Collections.Dictionary<string,Variant> message = null)
+	{
+		EmitSignal(nameof(StateStart));
+	}
+
+
+	public virtual void OnPhysicsUpdate(double delta)
+	{
+		EmitSignal(nameof(StatePhysicsUpdate));
+	}
+
+	public virtual void OnExit()
+	{
+		EmitSignal(nameof(StateExit));
+	}
+
+	protected virtual void SwitchState(string stateName, Godot.Collections.Dictionary<string,Variant> message = null)
+	{
+		EmitSignal(nameof(SwitchState), stateName, message);
+	}
+}
---
+++ Shared/StateMachine/StateMachine.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class StateMachine : Node
+{
+	[Export]
+	public State InitialState;
+	private State CurrentState;
+	private string PreviousStateName;
+	private Dictionary<string, State> States;
+
+
+	public override void _Ready()
+	{
+		if (InitialState is null)
+			return;
+
+		foreach (var state in GetChildren().OfType<State>())
+		{
+			var stateName = state.GetType().Name;
+            state.SwitchState += OnSwitchState;
+			States.Add(stateName, state);
+			GD.Print(stateName);
+		}
+
+		CurrentState = InitialState;
+		CurrentState.OnEnter();
+
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (CurrentState is null)
+			return;
+
+		CurrentState.OnPhysicsUpdate(delta);
+	}
+
+	public void OnSwitchState(string stateName, Godot.Collections.Dictionary<string,Variant> message = null)
+	{
+		var state = States[stateName];
+		if (state is null || state == CurrentState)
+			return;
+
+		PreviousStateName = CurrentState.GetType().Name;
+		CurrentState.OnExit();
+
+		CurrentState = state;
+		CurrentState.OnEnter(message);
+	}
+}
