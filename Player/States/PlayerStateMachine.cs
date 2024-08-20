93fd81b6e0b9463a0065159768caeabb19ba0658
zawette <zawette@gmail.com>
2024-08-20 02:03:40 +0200

    comment

--- Player/States/PlayerStateMachine.cs
+++ Player/States/PlayerStateMachine.cs
@@ -6,7 +6,7 @@ public partial class PlayerStateMachine : StateMachine
 	public Label DebugState;
 	public override async void _Ready()
 	{
-		await ToSignal(Owner, SignalName.Ready);
+		await ToSignal(Owner, SignalName.Ready); //waiting for player to be ready
 		base._Ready();
 		DebugState = Owner.GetNode<Label>("DebugState");
 	}
