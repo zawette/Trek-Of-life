3488989fcad6518825bc34f819da42f102e1dfb9
zawette <zawette@gmail.com>
2024-08-20 15:30:52 +0200

    removing unused enum

--- Player/States/BasePlayerState.cs
+++ Player/States/BasePlayerState.cs
@@ -5,7 +5,6 @@ public partial class BasePlayerState : State
 {
     [Export] public Player PlayerV;
     public enum playerMsgKeys {
-        freeFall,
         wallJump,
     }
     public override void _Ready()
