bf438121565858ec1157b3a3fc6f6ad0e4fb7c25
zawette <zawette@gmail.com>
2024-08-21 15:49:00 +0200

    layers and hitboxes

--- Player/weapons/bullet/Bullet.cs
+++ Player/weapons/bullet/Bullet.cs
@@ -6,15 +6,31 @@ namespace Player.weapons;
 public partial class Bullet : Node2D
 {
     private VisibleOnScreenNotifier2D _visibleOnScreenEnabler2D;
+    private Area2D _hitBox;
+
     [Export] public float Speed = 20f;
     public Vector2 Direction { get; set; }
 
     public override void _Ready()
     {
         _visibleOnScreenEnabler2D = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
+        _hitBox = GetNode<Area2D>("Hitbox");
         _visibleOnScreenEnabler2D.ScreenExited += OnScreenExited;
-        //TODO: when bullet hits target, make it disappear
+        _hitBox.AreaEntered += OnAreaEntered;
+        _hitBox.BodyEntered += OnBodyEntered;
     }
+
+    private void OnBodyEntered(Node2D body)
+    {
+        QueueFree();
+    }
+
+
+    private void OnAreaEntered(Area2D area)
+    {
+         area.GetParent().QueueFree();
+    }
+
 
     private void OnScreenExited()
     {
